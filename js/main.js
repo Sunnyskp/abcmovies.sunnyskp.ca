0b911bbaa131af4b088a202e6d3817181e07a157
Sunnyskp <skp2june@gmail.com>
2019-06-15 12:28:56 -0400

    Pagination and First modal working with errors

--- js/main.js
+++ js/main.js
@@ -7,6 +7,7 @@ const pageList2= document.querySelector("#pageList2");
 //To be able to send HTTP request to an external server. You must create the below object
 const xhr = new XMLHttpRequest();
 const xhr2 = new XMLHttpRequest();
+const xhr3 = new XMLHttpRequest();
 const displayPosts = () =>
 {
 
@@ -32,14 +33,14 @@ const populateValues=()=>
 {
     if(xhr.readyState==4)
     {
-      const jasonData=JSON.parse(xhr.responseText);
-     for (let index = 0; index < jasonData.results.length; index++) {
-      let movieId=jasonData.results[index].id;
-      let movieDescription = jasonData.results[index].overview;
+      const jasonData1=JSON.parse(xhr.responseText);
+     for (let index = 0; index < jasonData1.results.length; index++) {
+      let movieId=jasonData1.results[index].id;
+      let movieDescription = jasonData1.results[index].overview;
       let movieDescriptionSubstring= movieDescription.substring(0, 135);
-      let dateString = new Date(jasonData.results[index].release_date);
+      let dateString = new Date(jasonData1.results[index].release_date);
       let dateValue = dateString.toDateString();
-        let movieImage = `https://image.tmdb.org/t/p/original/${jasonData.results[index].poster_path}`
+        let movieImage = `https://image.tmdb.org/t/p/original/${jasonData1.results[index].poster_path}`
         movieLists.innerHTML += `<div Id=${movieId} class="column-style column is-mobile is-tablet is-desktop is-widescreen is-fullhd">
         <article class="media">
           <figure class="media-left">
@@ -49,7 +50,7 @@ const populateValues=()=>
           </figure>
           <div  class="media-content">
             <div class="content">
-                <strong>${jasonData.results[index].title}</strong><br><small><i>Release Date:</i> <strong>${dateValue}</strong><br><i>Movie Rating:</i> <strong>${jasonData.results[index].vote_average}/10</strong></small>
+                <strong>${jasonData1.results[index].title}</strong><br><small><i>Release Date:</i> <strong>${dateValue}</strong><br><i>Movie Rating:</i> <strong>${jasonData1.results[index].vote_average}/10</strong></small>
                 <div align="justify" class="div-para-style">${movieDescriptionSubstring}...<a onclick="displayModal()"><i>Read more...</i></a></div>
             </div>
           </div>
@@ -60,11 +61,11 @@ const populateValues=()=>
       </div>`
      }  
 
-     for (let pg = 1; pg <= jasonData.total_pages; pg++) {
-      pageList1.innerHTML += `<li><a id=${pg} class="pagination-link" aria-label="Page ${pg}" aria-current="page">${pg}</a></li>`;
+     for (let pg = 1; pg <= jasonData1.total_pages; pg++) {
+      pageList1.innerHTML += `<li id=${pg} ><a class="pagination-link" aria-label="Page ${pg}" aria-current="page">${pg}</a></li>`;
      }
-     for (let pg =1; pg <= jasonData.total_pages; pg++) {
-      pageList2.innerHTML += `<li><a id=${pg} class="pagination-link" aria-label="Page ${pg}" aria-current="page">${pg}</a></li>`;
+     for (let pg =1; pg <= jasonData1.total_pages; pg++) {
+      pageList2.innerHTML += `<li id=${pg} ><a class="pagination-link" aria-label="Page ${pg}" aria-current="page">${pg}</a></li>`;
      }
     }
 }
@@ -74,23 +75,38 @@ movieLists.addEventListener(`click`, (event) => {
   if (!selectedMovie) return;
   let selectedMovieId= selectedMovie.id;
 displayModal(selectedMovieId);
-getselectedMoviesAsHTML(selectedMovieId);
+getSelectedMoviesAsHTML(selectedMovieId);
 });
 
-const getselectedMoviesAsHTML = (receivedMovieId) => {
+pageList1.addEventListener(`click`, (event) => {
+  let selectedPage = event.target.closest(`li`);
+  if (!selectedPage) return;
+  let selectedPageId= selectedPage.id;
+getSelectedPage(selectedPageId);
+});
+
+const getSelectedMoviesAsHTML = (receivedMovieId) => {
   const movieEndPoint=`https://api.themoviedb.org/3/movie/${receivedMovieId}/videos?api_key=f064c905e826d5e04ed8a01dfa803649&language=en-US`
   xhr2.open("GET",movieEndPoint);
   xhr2.send();
   xhr2.addEventListener("readystatechange",displayModal)  
      }; 
 
+const getSelectedPage = (receivedPageId) => {
+  const pageEndPoint=`https://api.themoviedb.org/3/movie/now_playing?api_key=f064c905e826d5e04ed8a01dfa803649&language=en-US&page=${receivedPageId}`
+  xhr3.open("GET",pageEndPoint);
+  xhr3.send();
+  xhr3.addEventListener("readystatechange",displaySelectedPage)  
+
+}
+
 const displayModal = (receivedMovieId) =>
 {
-  if(xhr2.readyState==4 && xhr2.readyState==4)
+  if(xhr2.readyState==4 && xhr2.readyState==4  || xhr3.readyState==4 )
     {
-  const jasonData=JSON.parse(xhr.responseText);
+  const jasonData1=JSON.parse(xhr.responseText);
   const jasonData2=JSON.parse(xhr2.responseText);
-  let obj = jasonData.results.find(obj => obj.id == `${receivedMovieId}`);
+  let obj = jasonData1.results.find(obj => obj.id == `${receivedMovieId}`);
   movieLists.innerHTML += `<div class="modal is-active">
   <div class="modal-background"><img class="modalBgImage" src="https://image.tmdb.org/t/p/original/${obj.backdrop_path}"></div>
   <div class="modal-card modal-card-style">
@@ -113,7 +129,7 @@ const displayModal = (receivedMovieId) =>
   </article>
   <div>
   <div>
-  <div class="trailerBg"><iframe  class="trailerFrame" width="600px" height="520px" src="https://www.youtube.com/embed/${jasonData2.results[0].key}" frameborder="0" allow="accelerometer; autoplay; encrypted-media;" allowfullscreen></iframe></div>
+  <iframe  class="trailerFrame" width="600px" height="520px" src="https://www.youtube.com/embed/${jasonData2.results[0].key}" frameborder="0" allow="accelerometer; autoplay; encrypted-media;" allowfullscreen></iframe>
   </div></section>
     <footer class="modal-card-foot">
       <button onclick="location.href = '../index.html';" class="button is-link">Go Back to Home Page</button>
@@ -123,4 +139,77 @@ const displayModal = (receivedMovieId) =>
 }
 }
 
+const displaySelectedPage = () =>{
 
+  if(xhr3.readyState==4)
+    {
+  const jasonData3=JSON.parse(xhr3.responseText);
+  movieLists.innerHTML =``;
+  for (let index = 0; index < jasonData3.results.length; index++) {
+    let movieId=jasonData3.results[index].id;
+    let movieDescription = jasonData3.results[index].overview;
+    let movieDescriptionSubstring= movieDescription.substring(0, 135);
+    let dateString = new Date(jasonData3.results[index].release_date);
+    let dateValue = dateString.toDateString();
+    let movieImage = `https://image.tmdb.org/t/p/original/${jasonData3.results[index].poster_path}`;
+      movieLists.innerHTML += `<div Id=${movieId} class="column-style column is-mobile is-tablet is-desktop is-widescreen is-fullhd">
+      <article class="media">
+        <figure class="media-left">
+          <p class="image is-128x128">
+          <img src="${movieImage}"><br>
+          </p>
+        </figure>
+        <div  class="media-content">
+          <div class="content">
+              <strong>${jasonData3.results[index].title}</strong><br><small><i>Release Date:</i> <strong>${dateValue}</strong><br><i>Movie Rating:</i> <strong>${jasonData3.results[index].vote_average}/10</strong></small>
+              <div align="justify" class="div-para-style">${movieDescriptionSubstring}...<a onclick="displayModal()"><i>Read more...</i></a></div>
+          </div>
+        </div>
+      </article>
+    <div class="moreInfo">
+    <a onclick="displayModalSelectedPage()"><i><strong>More Info...</strong></i></a>
+    </div>
+    </div>`
+  }
+
+    }
+
+}
+
+const displayModalSelectedPage = () =>{
+  if(xhr3.readyState==4 && xhr2.readyState==4)
+    {
+  const jasonData1=JSON.parse(xhr3.responseText);
+  const jasonData2=JSON.parse(xhr2.responseText);
+  let obj = jasonData1.results.find(obj => obj.id == `${receivedMovieId}`);
+  movieLists.innerHTML += `<div class="modal is-active">
+  <div class="modal-background"><img class="modalBgImage" src="https://image.tmdb.org/t/p/original/${obj.backdrop_path}"></div>
+  <div class="modal-card modal-card-style">
+  <header class="modal-card-head">
+  <strong>${obj.title}</strong>
+</header>
+    <section class="modal-card-body">
+    <article class="media" Id=${receivedMovieId}>
+    <figure class="media-left">
+      <p class="image is-128x128">
+      <img src="https://image.tmdb.org/t/p/original/${obj.poster_path}"><br>
+      </p>
+    </figure>
+    <div  class="media-content">
+      <div class="content">
+    <small><i>Release Date:</i> <strong>${obj.release_date}</strong><br><i>Movie Rating:</i> <strong>${obj.vote_average}/10</strong></small>
+          <div align="justify" class="div-para-style"><strong>${obj.overview}</strong></div>
+       </div>
+    </div>
+  </article>
+  <div>
+  <div>
+  <iframe  class="trailerFrame" width="600px" height="520px" src="https://www.youtube.com/embed/${jasonData2.results[0].key}" frameborder="0" allow="accelerometer; autoplay; encrypted-media;" allowfullscreen></iframe>
+  </div></section>
+    <footer class="modal-card-foot">
+      <button onclick="location.href = '../index.html';" class="button is-link">Go Back to Home Page</button>
+    </footer>
+  </div>
+</div>`;  
+}
+}
